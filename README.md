cbf52b75f352d57799bbc2c6c7d54289c1a44cd5
BartSoj <bartsoj@gmail.com>
2022-12-08 20:18:37 +0100

    restructure project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Textbook to pdf
+
 converts online textbooks from websites like wsipnet.pl or nowaera.pl to pdfs
+
+## Usage
+
+* fill `textbook_urls.json` with textbook names and urls, `{}` in page url indicates page number
+* fill `cookies.txt` with cookies that are required in order to download textbook
+* check `textbook_to_pdf.py` -> `download_image` function to edit program specification
+* run `python textbook_to_pdf.py` in your terminal
+
+Your textbook will be saved in the same folder which you run the program
--- textbook_to_pdf.py
+++ textbook_to_pdf.py
@@ -8,6 +8,20 @@ import json
 requests.packages.urllib3.disable_warnings()
 
 
+def get_textbook_urls(filename="textbook_urls.json"):
+    with open(filename) as file:
+        textbooks = json.load(file)
+        return textbooks
+
+
+def get_cookies(filename="cookies.txt"):
+    with open(filename) as file:
+        cookies = ";".join(file.read().split("\n"))
+        cookies = [cookie.split("=") for cookie in cookies.split(";") if "=" in cookie]
+        cookies = {key: value for key, value in cookies}
+        return cookies
+
+
 def generate_dir(name="textbook"):
     dir_i = ""
     dir_path = os.path.join(os.getcwd(), name)
@@ -22,44 +36,63 @@ def generate_dir(name="textbook"):
     return dir_path + str(dir_i)
 
 
-cookies = {
-    #  change your code here
-    "auth_cookie": r"cookie_content"
-}
+def imgs_to_pdf(pdf_path, imgs):
+    if not pdf_path.endswith(".pdf"):
+        pdf_path += ".pdf"
 
-with open("textbook_urls.json") as file:
-    textbooks = json.load(file)
-
-for textbook, url in textbooks.items():
-    dir_path = generate_dir(textbook)
-
-    print(f"downloading {textbook} images from {url} to {dir_path}")
-
-    imgs = []
-
-    i = 0
-    while True:
-        i += 1
-
-        #  sleep(0.5)  # uncomment if you get connection time out
-
-        page = str(i)
-        #  page = page.zfill(3)  # uncomment to fill page number with leading zeros
-
-        resp = requests.get(url.format(page), cookies=cookies, verify=False, stream=True)
-
-        if resp.status_code != 200 or resp.headers["Content-Type"] != "image/jpeg":  # make sure your page url returns correct status code and content type
-            break
-
-        img_path = f"{dir_path}/image{i}.jpg"
-        with open(img_path, 'wb') as file:
-            resp.raw.decode_content = True
-            shutil.copyfileobj(resp.raw, file)
-
-        imgs.append(img_path)
-        print(f"image{i}.jpg saved")
-
-    with open(f"{dir_path}.pdf", "wb") as file:
+    with open(pdf_path, "wb") as file:
         file.write(img2pdf.convert(imgs))
 
-    print(f"textbook {textbook} saved as {dir_path}.pdf")
+
+def download_image(url, page, img_dir):
+    #  sleep(0.5)  # uncomment if you get connection timeout
+
+    page = str(page)
+    #  page = page.zfill(3)  # uncomment to fill page number with leading zeros
+
+    cookies = get_cookies()
+    resp = requests.get(url.format(page), cookies=cookies, verify=False, stream=True)
+
+    # make sure your page url returns correct status code and content type
+    if resp.status_code != 200 or resp.headers["Content-Type"] != "image/jpeg":
+        return
+
+    img_path = f"{img_dir}/image{page}.jpg"
+    with open(img_path, 'wb') as file:
+        resp.raw.decode_content = True
+        shutil.copyfileobj(resp.raw, file)
+
+    return img_path
+
+
+def run():
+    textbooks = get_textbook_urls()
+
+    for textbook, url in textbooks.items():
+        textbook_dir = generate_dir(textbook)
+
+        print(f"downloading {textbook} images from {url} to {textbook_dir}")
+
+        imgs = []
+        page = 1
+        while True:
+            img_path = download_image(url, page, textbook_dir)
+
+            if img_path:
+                imgs.append(img_path)
+                page += 1
+                print(f"{img_path} saved")
+            else:
+                if page > 1:
+                    print(f"finished, saved {page - 1} pages")
+                    break
+                else:
+                    print("textbook cannot be downloaded from this url")
+                    return
+
+        imgs_to_pdf(textbook_dir, imgs)
+        print(f"textbook {textbook} saved as {textbook_dir}.pdf")
+
+
+if __name__ == "__main__":
+    run()
